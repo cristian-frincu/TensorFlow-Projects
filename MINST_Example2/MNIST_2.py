ec8ae614907dc1c7edc236205a6d2cafb8a9bc59
Cristian Frincu <ccfrincu@gmail.com>
2015-11-10 23:56:11 -0500

    NOT working TensorBoard :(
    I am pretty close to getting it right though

--- MINST_Example2/MNIST_2.py
+++ MINST_Example2/MNIST_2.py
@@ -2,11 +2,16 @@ import input_data
 import tensorflow as tf
 
 
+
+
 print "Program started..."
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 print "Done loading data"
 
 sess = tf.InteractiveSession()
+
+
+
 x = tf.placeholder("float", shape=[None, 784])
 y_ = tf.placeholder("float", shape=[None, 10])
 
@@ -96,13 +101,13 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 sess.run(tf.initialize_all_variables())
-for i in range(20000):
-  batch = mnist.train.next_batch(50)
-  if i%100 == 0:
-    train_accuracy = accuracy.eval(feed_dict={
+for i in range(400):
+    batch = mnist.train.next_batch(50)
+    if i%100 == 0:
+        train_accuracy = accuracy.eval(feed_dict={
         x:batch[0], y_: batch[1], keep_prob: 1.0})
-    print "step %d, training accuracy %g"%(i, train_accuracy)
-  train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+        print "step %d, training accuracy %g"%(i, train_accuracy)
+    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
 print "test accuracy %g"%accuracy.eval(feed_dict={
     x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
---
+++ TensorBoard/TensorBoard.py
@@ -0,0 +1,62 @@
+import input_data
+import tensorflow as tf
+
+
+print "Start program"
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+print "Done loading images"
+
+x = tf.placeholder("float", [None, 784])
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
+
+y = tf.nn.softmax(tf.matmul(x,W) + b)
+
+y_ = tf.placeholder("float", [None,10])
+
+print "Done init"
+
+cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+
+print "Done loading vars"
+
+init = tf.initialize_all_variables()
+print "Done init vars"
+
+sess = tf.Session()
+sess.run(init)
+
+print '------'
+print type(sess.graph_def)
+
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+acc = tf.scalar_summary("Accuracy:", accuracy)
+
+merged_summary_op = tf.merge_all_summaries()
+summary_writer = tf.train.SummaryWriter('/Users/Cristian/Desktop/log', sess.graph)
+
+print "Session started"
+
+
+for i in range(2000):
+  batch_xs, batch_ys = mnist.train.next_batch(100)
+  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+  if i % 100 == 0:
+    print sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+    summary_str = sess.run(merged_summary_op)
+    summary_writer.add_summary(summary_str, i)
+
+
+print "Done training"
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+
+print sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
